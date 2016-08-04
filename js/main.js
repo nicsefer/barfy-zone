43fa104501c306483c216ee6b85a1b290d29073d
Alvaro Castro <moltar89@gmail.com>
2016-08-04 14:10:22 -0300

    .

--- js/main.js
+++ js/main.js
@@ -1,16 +1,46 @@
-var currentVersion;
+var currentVersion,
+	audios = [];
 
 $(function () {
 	$.getJSON('/package.json', function (data) {
 		currentVersion = data.version;
-		$('h1 small').text(currentVersion);
-		setupConnection();
+		$('header small').text(currentVersion);
+
+		getContents(function (data) {
+			preloadAudios(data.audio);
+			setupConnection();
+		});
 	});
 });
 
-function setupConnection () {
+setInterval(function () {
+	$.getJSON('/package.json', function (data) {
+		if (data.version !== currentVersion) {
+			window.location.reload();
+		}
+	});
+}, 60 * 5 * 1000);
+
+var getContents = function (cb) {
+	$.getJSON('/contents.json', cb);
+};
+
+var preloadAudios = function (audios) {
+	audios.forEach(function (audio) {
+		$('body').append('<audio preload="true"><source  src="/assets/audio/' + audio + '.mp3" type="audio/mpeg"></audio>');
+	});
+};
+
+var setupConnection = function () {
 	var HOST = 'wss://barfy-server.herokuapp.com',
 		ws = new WebSocket(HOST);
+
+	ws.onopen = function () {
+		ws.send(JSON.stringify({
+			type: 'handshake',
+			content: audios
+		}));
+	};
 
 	ws.onmessage = function (event) {
 		var data = JSON.parse(event.data),
@@ -40,12 +70,4 @@ function setupConnection () {
 	ws.onclose = function (event) {
 		setupConnection();
 	}
-}
-
-setInterval(function () {
-	$.getJSON('/package.json', function (data) {
-		if (data.version !== currentVersion) {
-			window.location.reload();
-		}
-	});
-}, 60 * 5 * 1000);
+};
